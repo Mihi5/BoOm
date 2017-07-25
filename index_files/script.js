657d28d3058cc82a5bc95190d1767259e8a4c7e3
mihi5 <mihaela.zhelezarova@gmail.com>
2017-07-25 12:19:15 +0200

    improve menu

--- index_files/script.js
+++ index_files/script.js
@@ -1,6 +1,17 @@
+
+var a = 0;
 function openNav() {
+  if(a === 0){
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    a = 1;
+  }
+  else{
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    a = 0;
+  }
+
 }
 
 function closeNav() {
